7205bf9acb3b43449f61fd44bf537017746e9b78
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-03-29 22:28:06 +0900

    Fix update URL checking

--- src/main/java/net/eve0415/ifpatcher/Reference.java
+++ src/main/java/net/eve0415/ifpatcher/Reference.java
@@ -6,4 +6,5 @@ public class Reference {
     public static final String VERSION = "@VERSION@";
     public static final String DEPENDENCIES = "required:industrialforegoing@[1.12.2-1.12.2];";
     public static final String FINGERPRINT = "@FINGERPRINT@";
+    public static final String UPDATEJSON = "https://raw.githubusercontent.com/eve0415/IFPatcher/main/update.json";
 }
