4e905d166a6f25d9da49b63bf2d2c68ab33afbd2
eve0415 <40632436+eve0415@users.noreply.github.com>
2024-04-08 09:18:27 +0900

    2.4.0

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 # IFPatcher [![Build](https://github.com/eve0415/IFPatcher/actions/workflows/build.yml/badge.svg)](https://github.com/eve0415/IFPatcher/actions/workflows/build.yml) [![Release](https://img.shields.io/github/v/release/eve0415/IFPatcher)](https://github.com/eve0415/IFPatcher/releases/latest)
 
-As Industrial Foregoing for 1.12.2 is EOL, this is a project to patch some known issues with [Industrial Foregoing](https://www.curseforge.com/minecraft/mc-mods/industrial-foregoing) for 1.12.2.
+As Industrial Foregoing for 1.12.2 is EOL, this is a project to patch some known issues
+with [Industrial Foregoing](https://www.curseforge.com/minecraft/mc-mods/industrial-foregoing) for 1.12.2.
 
 *It will not work on any other versions.*
 
-Please use the latest version [(1.12.13-237)](https://www.curseforge.com/minecraft/mc-mods/industrial-foregoing/files/2745321) to use this patch.
+Please use the latest
+version [(1.12.13-237)](https://www.curseforge.com/minecraft/mc-mods/industrial-foregoing/files/2745321) to use this
+patch.
 
 ## Features
 
 This mod will patch some bugs or implement some features:
 
 - Insertion Conveyor Upgrade
-  - Will not duplicate items when using multiple insertion upgrade on one conveyor belt.
+    - Will not duplicate items when using multiple insertion upgrade on one conveyor belt.
 - Fluid Pump
-  - Make pump work properly on any conditions.
-  - Can now fill fluid to buckets directly from GUI.
+    - Make pump work properly on any conditions.
+    - Can now fill fluid to buckets directly from GUI.
 - Laser Base
-  - Stop crashing Minecraft when using negative lens.
+    - Stop crashing Minecraft when using negative lens.
 - Potion Brewer
-  - Can now brew potion that the ingredients does not start from `minecraft:nether_wart`. (Ex. Potion of Weakness)
+    - Can now brew potion that the ingredients does not start from `minecraft:nether_wart`. (Ex. Potion of Weakness)
 - Plant Sower
-  - Till any blocks if possible. (You no longer have to use `minecraft:dirt` or `minecraft:grass`)
+    - Till any blocks if possible. (You no longer have to use `minecraft:dirt` or `minecraft:grass`)
 - Mob Imprisonment Tool
-  - Implement blacklist for mobs that can't be imprisoned.
-    - Support wildcard `*`. (Ex. `minecraft:*`)
+    - Implement blacklist for mobs that can't be imprisoned.
+        - Support wildcard `*`. (Ex. `minecraft:*`)
+    - Add option to enable/disable of imprisoning boss mobs. (`blacklistBosses`)
+        - Even if `blacklistBosses` is set to `false`, you can't imprison boss mobs if it is in the blacklist.
 - Latex Processing Unit
     - Will not eat `Latex Bucket` when tank is already full enough to fill.
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,30 +3,30 @@ import net.minecraftforge.gradle.userdev.UserDevExtension
 import java.util.*
 
 plugins {
-  java
+    java
     kotlin("jvm") version "1.9.23"
     id("net.kyori.blossom") version "2.1.0"
 }
 
 buildscript {
-  repositories {
-    maven { url = uri("https://repo.siro256.dev/repository/maven-public/") }
-  }
-
-  dependencies {
-      classpath("net.minecraftforge.gradle:ForgeGradle:[6.0,6.2)") {
-      isChanging = true
+    repositories {
+        maven { url = uri("https://repo.siro256.dev/repository/maven-public/") }
     }
-  }
+
+    dependencies {
+        classpath("net.minecraftforge.gradle:ForgeGradle:[6.0,6.2)") {
+            isChanging = true
+        }
+    }
 }
 
 apply(plugin = "net.minecraftforge.gradle")
 
 repositories {
-  mavenCentral()
-  gradlePluginPortal()
-  maven(url = "https://maven.minecraftforge.net")
-  maven(url = "https://plugins.gradle.org/m2/")
+    mavenCentral()
+    gradlePluginPortal()
+    maven(url = "https://maven.minecraftforge.net")
+    maven(url = "https://plugins.gradle.org/m2/")
     maven(url = "https://cursemaven.com") {
         content {
             includeGroup("curse.maven")
@@ -35,7 +35,7 @@ repositories {
 }
 
 group = "net.eve0415"
-version = "2.3.1-SNAPSHOT"
+version = "2.4.0"
 
 java {
     toolchain {
@@ -44,29 +44,29 @@ java {
 }
 
 val signProps = if (!System.getenv("KEY_STORE").isNullOrEmpty()) {
-  System.getenv("KEY_STORE").reader().let {
-    val prop = Properties()
-    prop.load(it)
-    return@let prop
-  }
+    System.getenv("KEY_STORE").reader().let {
+        val prop = Properties()
+        prop.load(it)
+        return@let prop
+    }
 } else if (file("secret.properties").exists()) {
-  file("secret.properties").inputStream().let {
-    val prop = Properties()
-    prop.load(it)
-    return@let prop
-  }
+    file("secret.properties").inputStream().let {
+        val prop = Properties()
+        prop.load(it)
+        return@let prop
+    }
 } else {
-  Properties()
+    Properties()
 }
 
 configure<UserDevExtension> {
-  mappings("snapshot", "20180814-1.12")
+    mappings("snapshot", "20180814-1.12")
 }
 
 dependencies {
-  "minecraft"("net.minecraftforge:forge:1.12.2-14.23.5.2860")
-  implementation("curse.maven:industrialforegoing-266515:2745321")
-  implementation("curse.maven:teslacorelib-254602:3438487")
+    "minecraft"("net.minecraftforge:forge:1.12.2-14.23.5.2860")
+    implementation("curse.maven:industrialforegoing-266515:2745321")
+    implementation("curse.maven:teslacorelib-254602:3438487")
     implementation(kotlin("stdlib-jdk8"))
 }
 
@@ -82,52 +82,52 @@ sourceSets {
 }
 
 tasks {
-  compileJava {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
-  }
-
-  named<ProcessResources>("processResources") {
-    inputs.property("version", project.version)
-    from(sourceSets.main.get().resources.srcDirs) {
-      duplicatesStrategy = DuplicatesStrategy.INCLUDE
-      include("mcmod.info")
-      expand("version" to project.version)
-    }
-  }
-
-  named<Jar>("jar") {
-    manifest {
-      attributes(
-        mapOf(
-          "Specification-Title"         to "IFPatcher",
-          "Specification-Vendor"        to "eve0415",
-          "Specification-Version"       to "1", // We are version 1 of ourselves
-          "Implementation-Title"        to project.name,
-          "Implementation-Version"      to project.version,
-          "Implementation-Vendor"       to "eve0415",
-          "FMLCorePlugin"               to "net.eve0415.ifpatcher.IFPatcher",
-          "FMLCorePluginContainsFMLMod" to "true",
-        )
-      )
-    }
-  }
-
-  create<SignJar>("signJar") {
-    dependsOn("reobfJar")
-    onlyIf {
-      signProps.isNotEmpty()
+    compileJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
     }
 
-    keyStore.set(signProps["keyStore"] as String?)
-    storePass.set(signProps["keyStorePass"] as String?)
-    alias.set(signProps["keyStoreAlias"] as String?)
-    keyPass.set(signProps["keyStoreKeyPass"] as String?)
-    inputFile.set(named<Jar>("jar").get().archiveFile)
-    outputFile.set(named<Jar>("jar").get().archiveFile)
-  }
+    named<ProcessResources>("processResources") {
+        inputs.property("version", project.version)
+        from(sourceSets.main.get().resources.srcDirs) {
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+            include("mcmod.info")
+            expand("version" to project.version)
+        }
+    }
 
-  named("build") {
-    dependsOn("signJar")
-  }
+    named<Jar>("jar") {
+        manifest {
+            attributes(
+                mapOf(
+                    "Specification-Title" to "IFPatcher",
+                    "Specification-Vendor" to "eve0415",
+                    "Specification-Version" to "1", // We are version 1 of ourselves
+                    "Implementation-Title" to project.name,
+                    "Implementation-Version" to project.version,
+                    "Implementation-Vendor" to "eve0415",
+                    "FMLCorePlugin" to "net.eve0415.ifpatcher.IFPatcher",
+                    "FMLCorePluginContainsFMLMod" to "true",
+                )
+            )
+        }
+    }
+
+    create<SignJar>("signJar") {
+        dependsOn("reobfJar")
+        onlyIf {
+            signProps.isNotEmpty()
+        }
+
+        keyStore.set(signProps["keyStore"] as String?)
+        storePass.set(signProps["keyStorePass"] as String?)
+        alias.set(signProps["keyStoreAlias"] as String?)
+        keyPass.set(signProps["keyStoreKeyPass"] as String?)
+        inputFile.set(named<Jar>("jar").get().archiveFile)
+        outputFile.set(named<Jar>("jar").get().archiveFile)
+    }
+
+    named("build") {
+        dependsOn("signJar")
+    }
 }
